c04cc6a73498e1d2ed3ad85c6cd2ce7adab25cca
Leonid <79872121399.yandex@gmail.com>
2022-08-08 21:15:45 +0300

    level generator

--- src/Level/Level.tsx
+++ src/Level/Level.tsx
@@ -1,36 +1,34 @@
 import css from './Level.module.sass'
 import cx from 'classnames';
 
-interface LevelProps {
-    gameRule?: {}
-}
+const Level = (gameRule?: {}) => {
+    // <div className={css.cellWrapper}>
+    //     <div style={{width: `50px`}} className={css.cell}></div>
+    //     <div className={cx(css.cell, css.active)}></div>
+    //     <div className={css.cell}></div>
+    // </div>
+    let areaInfo: any = [
+        ['active', 'full', 'empty', 'full', 'full', 'full'],
+        ['empty', 'full', 'full', 'full', 'empty', 'full'],
+        ['empty', 'empty', 'empty', 'empty', 'empty', 'full'],
+        ['empty', 'full', 'full', 'full', 'empty', 'full'],
+        ['empty', 'full', 'empty', 'full', 'empty', 'full'],
+        ['empty', 'full', 'empty', 'full', 'empty', 'full'],
+        ['empty', 'full', 'empty', 'full', 'full', 'full'],
+    ];
 
-const Level: React.FC<LevelProps> = (gameRule) => {
+    const Cell = (type: any) => {
+        if(type === 'empty') type = cx(css.cell, css.empty)
+        if(type === 'active') type = cx(css.cell, css.active)
+        if(type === 'full') type = css.cell
 
-    // const gameRule = {
-    //     playgroundArea: [2, 2, 2, 1],
-    //     startPoint: {x: 1, y: 1},
-    //     currentPoint: {x: 1, y: 1} //useState in future
-    // }
+        return <div className={type}></div>
+    }
 
     return (
         <div className={css.playgroundWrapper}>
             <div className={css.playgroundContentWrapper}>
-                <div className={css.ceilWrapper}>
-                    <div style={{width: `50px`}} className={css.cell}></div>
-                    <div className={css.cell}></div>
-                </div>
-                <div className={css.ceilWrapper}>
-                    <div className={css.cell}></div>
-                    <div className={css.cell}></div>
-                </div>
-                <div className={css.ceilWrapper}>
-                    <div className={css.cell}></div>
-                    <div className={css.cell}></div>
-                </div>
-                <div className={css.ceilWrapper}>
-                    <div className={cx(css.cell, css.active)}></div>
-                </div>
+                {areaInfo.map((item: Array<string>) => <div className={css.cellWrapper}>{item.map((item: string) => Cell(item))}</div>)}
             </div>
         </div>
     )
